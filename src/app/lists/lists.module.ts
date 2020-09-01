134bb035b08ffd9aa3c2105ec03577d8c950e1c1
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2020-09-01 17:49:15 -0600

    Added list name entry

--- src/app/lists/lists.module.ts
+++ src/app/lists/lists.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListsComponent } from './lists/lists.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared.module';
 
 @NgModule({
   declarations: [ListsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule, ReactiveFormsModule],
   exports: [ListsComponent]
 })
 export class ListsModule {}
--- src/app/lists/lists/lists.component.ts
+++ src/app/lists/lists/lists.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl
+} from '@angular/forms';
 
 import { ListsService } from './../lists.service';
 
@@ -8,9 +14,21 @@ import { ListsService } from './../lists.service';
   styleUrls: ['./lists.component.css']
 })
 export class ListsComponent implements OnInit {
-  constructor(private listsService: ListsService) {}
+  pageTitle = 'Add List';
+  createListForm: FormGroup;
+
+  constructor(private listsService: ListsService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.createListForm = this.fb.group({
+      listName: ['', [Validators.required]]
+    });
     // this.listsService.addList().subscribe(data => console.log(data));
+  }
+
+  onSubmit(): void {
+    const newListName = this.createListForm.value;
+    console.log(newListName);
+    // this.store.dispatch(UserActions.loginUser({ user }));
   }
 }
